61daa0d60b30d7b5cddf572cb97d6e080f69ce0c
Rudy Sorto <rudy.eli.ayala@gmail.com>
2016-12-07 13:00:37 -0600

    commit al nuevo branch con el error 1234

--- src/com/rudysorto/gitlocal/GitClass.java
+++ src/com/rudysorto/gitlocal/GitClass.java
@@ -2,8 +2,7 @@ package com.rudysorto.gitlocal;
 
 public class GitClass {
 	public static void main(String args[]){
-		//System.out.println("HW");
-		System.out.println("HW");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
